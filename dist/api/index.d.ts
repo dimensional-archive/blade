bcad7e5ecefe1485c9837453defd3783f193319d
LolWastedJS <ml2dofficial@gmail.com>
2020-06-13 12:49:14 -0700

    Move to a single typings file

--- dist/api/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "./stores";
-export * from "./components";
-export * from "./Client";
--- index.ts
+++ index.ts
@@ -1,23 +1,78 @@
-import { LiteEmitter } from "../../utils/LiteEmitter";
-import { Component, ComponentOptions } from "../components/Base";
-import { Storage } from "../../utils/Storage";
-import type { BladeClient } from "../Client";
-export declare type LoadFilter = (file: string) => boolean | Promise<boolean>;
-export declare type ComponentResolvable<T> = string | T;
-export interface ComponentStoreOptions {
-    classToHandle?: typeof Component;
-    priority?: number;
-    loadFilter?: LoadFilter;
-    autoCategory?: boolean;
-    defaults?: ComponentOptions;
-    createDirectory?: boolean;
-    directory?: string;
-}
-/**
- * A component store.
- * @since 1.0.0
- */
-export declare abstract class ComponentStore<T extends Component> extends LiteEmitter {
+import { Client, ClientOptions, Member, OAuthApplicationInfo, User } from "eris";
+import { Logger } from "@ayanaware/logger";
+
+declare module "@kyu/blade" {
+  /**
+   * The base class for creating a bot.
+   * @extends Client
+   * @since 1.0.0
+   */
+  export class BladeClient extends Client {
+    static basePermissions: (2048 | 1024)[];
+    /**
+     * This client's logger.
+     * @since 1.0.4
+     */
+    readonly logger: Logger;
+    /**
+     * A utility class for resolving different classes like Emoji and Member.
+     * @since 1.0.0
+     */
+    readonly util: ClientUtil;
+    /**
+     * Oauth2 Application Info.
+     * @since 1.0.0
+     */
+    app?: OAuthApplicationInfo;
+    /**
+     * The base directory of the bot.
+     * @since 1.0.0
+     */
+    directory: string;
+    /**
+     * Whether the client has been started or not.
+     * @since 1.0.0
+     */
+    started: boolean;
+    /**
+     * A set of owners.
+     */
+    owners: Set<User>;
+    options: BladeClientOptions;
+    /**
+     * A set of stores that are being used by the client.
+     * @since 1.0.0
+     */
+    private readonly _stores;
+    /**
+     * Creates a new BladeClient.
+     * @param options
+     */
+    constructor(options: BladeClientOptions);
+    get invite(): string | null;
+    /**
+     * Uses a store.
+     * @param store The store to use.
+     */
+    use(store: ComponentStore<Component>): this;
+    /**
+     * Starts the bot.
+     * @since 1.0.0
+     */
+    start(): Promise<this>;
+    /**
+     * Check if a member or user is an owner.
+     * @param resolvable The member/user to check.
+     * @since 1.0.0
+     */
+    isOwner(resolvable: User | Member): boolean;
+  }
+
+  /**
+   * A component store.
+   * @since 1.0.0
+   */
+  export abstract class ComponentStore<T extends Component> extends LiteEmitter {
     private static _defaults;
     /**
      * The client that is using this store.
@@ -98,4 +153,14 @@ export declare abstract class ComponentStore<T extends Component> extends LiteEm
      * @since 1.0.0
      */
     toString(): string;
+  }
+
+  /** Interfaces */
+
+  export interface BladeClientOptions extends ClientOptions {
+    directory?: string;
+    token: string;
+    owners?: string | string[];
+  }
+
 }
